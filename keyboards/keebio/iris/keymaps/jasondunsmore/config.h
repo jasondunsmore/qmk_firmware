2f10f6b9c75ead7f1787fe234fa31b62da840e72
Jason Dunsmore <jasondunsmore@gmail.com>
2020-01-07 15:42:15 -0600

    Add keypad layout

--- keyboards/keebio/iris/keymaps/jasondunsmore/config.h
+++ keyboards/keebio/iris/keymaps/jasondunsmore/config.h
@@ -19,5 +19,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 #define MASTER_LEFT
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 #define TAPPING_TOGGLE 2
--- keyboards/keebio/iris/keymaps/jasondunsmore/keymap.c
+++ keyboards/keebio/iris/keymaps/jasondunsmore/keymap.c
@@ -5,9 +5,11 @@ extern keymap_config_t keymap_config;
 // Layers
 #define _QWERTY 0 // Base layer
 #define _NAVI   1 // Navigation layer
+#define _NUMP   2 // Numpad layer
 
 // Keys
 #define KC_NAVI TT(_NAVI)
+#define KC_NUMP TT(_NUMP)
 #define KC_AGRV LALT_T(KC_GRAVE)
 #define KC_AQUO RALT_T(KC_QUOTE)
 #define KC_GUIE LGUI_T(KC_ESC)
@@ -36,13 +38,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
    F12,  F1,  F2,  F3,  F4,  F5,                 F6,  F7,  F8,  F9, F10, F11,
 //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-   TRNS,TRNS,TRNS,VOLU,TRNS,TRNS,               PGUP,HOME, UP, END, TRNS,TRNS,
+   TRNS,TRNS,MUTE,VOLU,TRNS,TRNS,               PGUP,HOME, UP, END, TRNS,TRNS,
 //|----+----+----+----+----+----|              |----+----+----+----+----+----|
    TRNS,TRNS,BRID,VOLD,BRIU,TRNS,               PGDN,LEFT,DOWN,RGHT,TRNS,TRNS,
 //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-   TRNS,TRNS,TRNS,MUTE,INS, TRNS,TRNS,     TRNS,TRNS,CAPS,LBRC,RBRC,TRNS,TRNS,
+   TRNS,TRNS,TRNS,NUMP,INS, TRNS,NUMP,     NUMP,TRNS,CAPS,LBRC,RBRC,TRNS,TRNS,
 //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                     TRNS,TRNS,TRNS,         TRNS,TRNS,TRNS
+                     NUMP,TRNS,TRNS,         TRNS,TRNS,NUMP
 //                  `----+----+----'        `----+----+----'
-      )
+     ),
+
+  [_NUMP] = LAYOUT_kc(
+//,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,               NLCK,TRNS,PSLS,PAST,PMNS,TRNS,
+//|----+----+----+----+----+----|              |----+----+----+----+----+----|
+   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS, P7,  P8,  P9, PPLS,TRNS,
+//|----+----+----+----+----+----|              |----+----+----+----+----+----|
+   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,               PEQL, P4,  P5,  P6, PDOT,TRNS,
+//|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS, P1,  P2,  P3, PCMM,TRNS,
+//`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                     TRNS,TRNS,TRNS,         PENT, P0, PDOT
+//                  `----+----+----'        `----+----+----'
+     )
 };
